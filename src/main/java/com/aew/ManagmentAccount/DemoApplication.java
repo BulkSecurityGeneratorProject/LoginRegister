0733b5e377b93e45fc66bcc2e38745d250770242
Adrian E. Wilgenhoff <adrianwilgenhoff@gmail.com>
2019-05-13 14:21:20 -0300

    add folders

---
+++ src/main/java/com/aew/ManagmentAccount/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.aew.ManagmentAccount;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class DemoApplication implements ApplicationRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+
+		logger.info("Creation of admin user");
+
+	}
+
+}
